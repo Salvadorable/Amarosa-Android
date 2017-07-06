3149898646b8b89e49b797af4d4c2bf9f09da26e
Brandon Seager <brandon.s.seager@wmich.edu>
2017-07-06 05:28:55 -0400

    did a few things

---
+++ Application/app/src/main/java/com/example/android/amarosa/CreateAccountPage.java
@@ -0,0 +1,13 @@
+package com.example.android.amarosa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccountPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_page);
+    }
+}
