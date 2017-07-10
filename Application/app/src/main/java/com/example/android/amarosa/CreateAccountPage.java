6d743978449a81a1fdb2cfa5ff0aa83160143280
Brandon Seager <brandon.s.seager@wmich.edu>
2017-07-09 22:14:51 -0400

    i have created a intent to move to the create account page'

--- Application/app/src/main/java/com/example/android/amarosa/CreateAccountPage.java
+++ Application/app/src/main/java/com/example/android/amarosa/CreateAccountPage.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class CreateAccountPage extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
