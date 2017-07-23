0f5cb384c65f5abb8bf6c51bee9a62b821bce6df
Brandon Seager <brandonseager@Brandons-MBP.attlocal.net>
2017-07-22 21:45:23 -0400

    added backend code to get ready to push new users to firebase

--- Application/app/src/main/java/com/example/android/amarosa/CreateAccountPage.java
+++ Application/app/src/main/java/com/example/android/amarosa/CreateAccountPage.java
@@ -1,14 +1,39 @@
 package com.example.android.amarosa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountPage extends AppCompatActivity {
 
-
+    private Button mFinishCreateAccountButton; //had to create my button
+    private Boolean accountVerified = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_page);
+
+        mFinishCreateAccountButton = (Button) findViewById(R.id.finish_create_account_button);
+        mFinishCreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                what do you want to happen after the finish button is clicked
+                this is the onclick listener
+                 */
+
+
+
+
+                if(accountVerified == true) {
+                    Intent i = new Intent(v.getContext(), ProfilePage.class);
+                    startActivity(i);
+                }
+            }
+        });
+
+
     }
 }
---
+++ Application/app/src/main/java/com/example/android/amarosa/ProfilePage.java
@@ -0,0 +1,22 @@
+package com.example.android.amarosa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by brandonseager on 7/22/17.
+ */
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+    }
+
+
+
+
+
+}
