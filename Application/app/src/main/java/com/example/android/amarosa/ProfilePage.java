1f00d5fc97d2a033b5141108a518a8bd6a97523c
parker loomis <bigdaddy@parkers-MacBook-Pro.local>
2017-07-24 18:17:59 -0400

    created profile page and settings page

--- Application/app/src/main/java/com/example/android/amarosa/ProfilePage.java
+++ Application/app/src/main/java/com/example/android/amarosa/ProfilePage.java
@@ -1,19 +1,31 @@
 package com.example.android.amarosa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by brandonseager on 7/22/17.
  */
 
 public class ProfilePage extends AppCompatActivity {
-
+    private Button mSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_page);
+        mSettings = (Button) findViewById(R.id.settings);
+        mSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),AccountSettings.class);
+                startActivity(i);
+            }
+        });//end of the intent
     }
+
 
 
 
